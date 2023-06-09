2ec2758ce67ae24bb5356f3628f1b9f61fe8edd3
kidrea <amauryperez2615@gmail.com>
2023-06-09 09:23:34 -0400

    feat: add functionality to find the user in the API

--- src/components/GitHubUser.tsx
+++ src/components/GitHubUser.tsx
@@ -3,17 +3,29 @@ import { useState } from 'react'
 
 export default function App() {
     const [enable, setEnable] = useState(false)
+    const [search, setSearch] = useState('')
     const [username, setUsername] = useState('')
-    const setUser = (event: any) => {
-        setUsername(event.target.value)
-    }
-    const handleClick = () => {
-        console.log(username)
+    const handleSubmit = async (e: any) => {
+        try {
+            e.preventDefault()
+            setEnable(true)
+            const response = await fetch(
+                `https://api.github.com/users/${search}`
+            )
+            const data = await response.json()
+            setUsername(data)
+        } catch (error: any) {
+            console.log('Error', error.message)
+        }
     }
     return (
         <>
             <div className="flex flex-row space-x-4">
-                <div className=" flex m-auto w-96 flex-col space-y-2 pt-2">
+                <form
+                    method="post"
+                    onSubmit={handleSubmit}
+                    className=" flex m-auto w-96 flex-col space-y-2 pt-2"
+                >
                     <div className=" pb-2 flex justify-center">
                         <a href="#">
                             <img
@@ -37,22 +49,21 @@ export default function App() {
                                 type="text"
                                 name="username"
                                 id="username_field"
-                                value={username}
-                                onChange={setUser}
+                                onChange={(e) => setSearch(e.target.value)}
                                 className="block shadow-black shadow-lg bg-black w-full rounded-md border-2 border-slate-600 py-1.5 pl-7 pr-20 text-slate-200  placeholder:text-slate-400  focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-blue-500 sm:text-sm sm:leading-6"
                                 placeholder="...usuario"
                             ></input>
                             <button
+                                type="submit"
                                 className=" h-9 shadow-black shadow-lg block bg-green-900 hover:bg-green-700 rounded-lg text-slate-200"
-                                onClick={handleClick}
                             >
                                 Buscar
                             </button>
                         </div>
                     </div>
-                </div>
+                </form>
 
-                <InfoPanel />
+                {enable ? <InfoPanel /> : null}
             </div>
         </>
     )
